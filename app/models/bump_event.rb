c3a2989948813fceffd9d898981b6dea306f3975
monkey codes <jz@monkey.codes>
2015-12-30 03:28:27 +0000

    Added named scopes

--- app/models/bump_event.rb
+++ app/models/bump_event.rb
@@ -1,2 +1,4 @@
 class BumpEvent < ActiveRecord::Base
+  scope :recent, ->(){where('created_at >= :time_ago',:time_ago => Time.now - 10.days) }
+  scope :nearby, ->(point){where('ST_Distance(lonlat, :point) < :distance',:point => point.as_text, :distance => 10)}
 end
